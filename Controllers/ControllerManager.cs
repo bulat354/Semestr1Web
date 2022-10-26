4d150b085ff6d56b004a98992f9eab8a6265b3b4
hafizovb <hbulat35@gmail.com>
2022-10-26 19:13:03 +0300

    Добавьте файлы проекта.

---
+++ Controllers/ControllerManager.cs
@@ -0,0 +1,57 @@
+﻿using MyServer.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace MyServer.Controllers
+{
+    public class ControllerManager
+    {
+        private static Dictionary<string, ControllerMethodInfo> methods;
+
+        static ControllerManager()
+        {
+            methods = Assembly.GetExecutingAssembly()
+                .GetTypes()
+                .Where(t => Attribute.IsDefined(t, typeof(ApiControllerAttribute)))
+                .SelectMany(t => ControllerMethodInfo.GetMethods(t))
+                .ToDictionary(c => c.Name, c => c);
+        }
+
+        public static HttpResponse MethodHandler(HttpListenerRequest request, Configs configs)
+        {
+            var segments = request.Url.Segments;
+            if (segments.Length < 2) return HttpResponse.GetNotFoundResponse();
+
+            string controllerName = segments[1].Replace("/", "").ToLower();
+            string httpMethodName = request.HttpMethod.ToLower();
+            string methodName = (segments.Length < 3)
+                ? ""
+                : segments[2].Replace("/", "").ToLower();
+
+            string fullName = $"{controllerName}.{methodName}.{httpMethodName}";
+
+            if (methods.TryGetValue(fullName, out var method))
+            {
+                var result = method.Invoke(request);
+
+                if (method.IsVoid)
+                    return HttpResponse.GetEmptyResponse();
+
+                if (result == null)
+                    return HttpResponse.GetNotFoundResponse();
+
+                return new HttpResponse(HttpStatusCode.OK, JsonSerializer.Serialize(result), "Application/json");
+            }
+            else
+            {
+                return HttpResponse.GetNotFoundResponse();
+            }
+        }
+    }
+}
---
+++ Controllers/ControllerMethodInfo.cs
@@ -0,0 +1,79 @@
+﻿using MyServer.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyServer.Controllers
+{
+    internal class ControllerMethodInfo
+    {
+        public readonly string Name;
+        public readonly bool IsVoid;
+
+        private readonly object? controller;
+        private readonly MethodInfo method;
+        private readonly HttpMethodAttribute httpAttribute;
+        private readonly ParameterInfo[] parameters;
+
+        public ControllerMethodInfo(object? controller, Type type, MethodInfo method)
+        {
+            if (Attribute.IsDefined(type, typeof(ApiControllerAttribute)))
+            {
+                this.controller = controller;
+
+                var controllerName = type.GetCustomAttribute<ApiControllerAttribute>().Name.ToLower();
+                if (controllerName == null)
+                    controllerName = type.Name.ToLower();
+
+                if (Attribute.IsDefined(method, typeof(HttpMethodAttribute)))
+                {
+                    this.method = method;
+                    httpAttribute = method.GetCustomAttribute<HttpMethodAttribute>();
+                    parameters = method.GetParameters();
+
+                    var methodName = httpAttribute.Name.ToLower();
+                    if (methodName.StartsWith('.'))
+                        methodName = method.Name.ToLower() + methodName;
+                    else if (methodName == null)
+                        methodName = method.Name.ToLower() + ".get";
+
+                    Name = $"{controllerName}.{methodName}";
+                    IsVoid = method.ReturnType == typeof(void);
+                }
+                else
+                    throw new ArgumentException("Method that was given doesn't have attribute HttpMethod");
+            }
+            else
+                throw new ArgumentException("Controller that was given doesn't have attribute HttpController");
+        }
+
+        public static IEnumerable<ControllerMethodInfo> GetMethods(Type controller)
+        {
+            var obj = Activator.CreateInstance(controller);
+            return controller.GetMethods()
+                .Where(m => Attribute.IsDefined(m, typeof(HttpMethodAttribute)))
+                .Select(m => new ControllerMethodInfo(obj, controller, m));
+        }
+
+        public object? Invoke(HttpListenerRequest request)
+        {
+            var strParams = httpAttribute.Parse(request);
+
+            object?[] objParams = new object?[parameters.Length];
+            for(int i = 0; i < parameters.Length; i++)
+            {
+                var param = parameters[i];
+                var str = strParams[param.Name];
+                if (str == null)
+                    return null;
+                objParams[i] = Convert.ChangeType(strParams[param.Name], param.ParameterType);
+            }
+
+            return method.Invoke(controller, objParams);
+        }
+    }
+}
